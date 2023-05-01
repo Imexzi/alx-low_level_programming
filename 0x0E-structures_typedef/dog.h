1e55c8877369a9e31f085a712027e97a5a18d1c1
Imisioluwa <adeniyiimisioluwa02@gmail.com>
2023-05-01 07:13:55 -0700

    dog functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,40 @@
+
 #ifndef DOG_H
 #define DOG_H
-/**
- * struct dog - function to be defined
- * @name: character1
- * @age: a float
- * @owner: character2
- * Description: This code is used to define a new type for dog with all element
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
-#endif
+	/**
+	 * struct dog - a dog's basic info
+	 * @name: First member
+	 * @age: Second member
+	 * @owner: Third member
+	 *
+	 * Description: Longer description
+	 */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
+
+
+	/**
+	 * dog_t - typedef for struct dog
+	 */
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+	#endif
+
+
+
+
